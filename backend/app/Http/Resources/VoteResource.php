fc9ab7527c05f5b1d60bfb789de9bc0b7102e0a7
Lyn <wenzhan.main@gmail.com>
2020-02-27 02:30:42 -0800

    fix vote and activity author show

--- backend/app/Http/Resources/VoteResource.php
+++ backend/app/Http/Resources/VoteResource.php
@@ -17,7 +17,7 @@ class VoteResource extends JsonResource
     {
         $author = [];
         $receiver = [];
-        if ($this->showUser($this)){
+        if ($this->showUser()){
             $author = new UserBriefResource($this->whenLoaded('author'));
             $receiver = new UserBriefResource($this->whenLoaded('receiver'));
         }
@@ -27,7 +27,7 @@ class VoteResource extends JsonResource
             'attributes' => [
                 'votable_type' => (string)$this->votable_type,
                 'votable_id' => (int)$this->votable_id,
-                'attitude' => (string)$this->attitude,
+                'attitude_type' => (string)$this->attitude_type,
                 'created_at' => (string)$this->created_at,
             ],
             'author' => $author,
@@ -35,16 +35,16 @@ class VoteResource extends JsonResource
         ];
     }
 
-    private function isUpvote($attitude){
-        return $attitude === 'upvote';
+    private function isUpvote($attitude_type){
+        return $attitude_type === 'upvote';
     }
 
     private function isOwnVote($user_id){
         return auth('api')->id()===$user_id;
     }
 
-    private function showUser($vote){
-        return $this->isUpvote($vote->attitude)||$this->isOwnVote($vote->user_id)||auth('api')->user()->isAdmin();
+    private function showUser(){
+        return $this->isUpvote($this->attitude_type)||$this->isOwnVote($this->user_id)||auth('api')->user()->isAdmin();
     }
 
 }
